d9b00db9b31362bcb18761f8fb5fc84ef7210ef1
Tausifqureshi786 <59664677+Tausifqureshi786@users.noreply.github.com>
2020-01-18 16:21:03 -0700

    Add files via upload
    These are a bunch of files that i have written with the help of the resources offered by our instructor from the D2L platform.

---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log("Hello World");
+
+const bob = "bob";
+let lotto = "00000000";
+
+lotto = 'dave'
+
+let myfriend = 'Bob';
+const arr = ["0",  "0" ]
+
+const obj = {
+    name: "Jane" 
+    age : 32,
+    gender: "female"
+    class: ["sodv1101", "tech1101"]
+};
+
+// console.log(bob, lotto);
+
