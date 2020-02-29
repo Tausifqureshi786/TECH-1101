b72e43a55376ae3633b7e322625f8809ec2f3fb0
Tausifqureshi786 <59664677+Tausifqureshi786@users.noreply.github.com>
2020-02-28 22:04:55 -0700

    Add files via upload

---
+++ Week 5/index.js
@@ -0,0 +1,40 @@
+
+
+// this adds the year to the copyright 
+
+
+ 
+
+function copyrightyear(){
+    copyright = document.querySelector("#copyright");
+    const D = new Date();
+    copyright.textContent =  D.getFullYear() + " " ;
+
+}
+// you should console.log
+console.log(copyright);
+copyrightyear();
+
+// let D = new Date();
+
+
+console.log("hii");
+
+// function that generates a random number
+
+function any(){
+    
+     const y = Math.floor(Math.random() *10) ;
+    console.log(y);
+}
+
+any();
+
+
+function () {
+
+}
+
+function () {
+
+}
